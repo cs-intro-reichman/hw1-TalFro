51ee1e340db21d4f74ead4b9380c4aed8f507638
Tal Froimovich <talfro@Tals-MacBook-Air.local>
2025-10-31 13:51:48 +0200

    HW1

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,22 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int num = Integer.parseInt(args[0]);
+		double a = num*Math.random();
+		double b = num*Math.random();
+		double c = num*Math.random();
+
+		System.out.println((int)a+" "+(int)b+" "+(int)c+" ");
+
+		double max = Math.max(a, b);
+		max = Math.max(max, c);
+		double min = Math.min(a, b);
+		min = Math.min(min,c);
+		double mid = a+b+c-max-min;
+
+		System.out.println((int)min+" "+(int)mid+" "+(int)max);
+
+
+
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,15 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int currentvalue = Integer.parseInt(args[0]);
+		double ribit = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
+
+		double futurevalue = currentvalue* Math.pow((1+ribit/100), years);
+
+		System.out.println("After " + years + "years , " + currentvalue + "$ saved at " + ribit + "% will yield " + futurevalue + "$");
+
+
+
 	}
 }
